91d98e3c940f8cd81eca1de7eb089e27b2ccd39a
balajee <balajeebalu22>
2021-01-16 22:28:38 +0530

    3rd commit

--- thestringcalculatorofjunittesting/src/test/java/thestringcalculatorofjunittesting/TestStringCalculator.java
+++ thestringcalculatorofjunittesting/src/test/java/thestringcalculatorofjunittesting/TestStringCalculator.java
@@ -11,6 +11,11 @@ public class TestStringCalculator{
 	return_0_when_input_is_empty() {
 		assertEquals("0", given(""));
 	}
+	@Test
+	public void
+	return_3_when_input_is_1_2() {
+		assertEquals("3", given("1,2"));
+	}
 
 	private String given(String number) {
 		StringCalculator stringCalculator = new StringCalculator();
